d659c619860a61f7a4e9845fc2fbb48f7c0d8c10
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-05-07 20:31:03 -0700

    set-up

---
+++ models/books.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: "The Shinobi Initiative",
+  description: "The reality-bending adventures of a clandestine service agency in the year 2166",
+  year: 2014,
+  quantity: 10,
+  imageURL: "/assets/shinobi-initiative.jpeg"
+});
+
+module.exports = mongoose.model('Book', bookSchema);
